d7a7fbe722dfd2311eb77ef70a052ee3da72de4c
Harsh <harshnag23@gmail.com>
2022-07-03 14:45:40 +0530

    Second Commit

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,11 +4,12 @@ import { useSession } from "next-auth/react";
 function Avatar() {
   const { data: session } = useSession();
   return (
-  <div className="relative h-10 w-10 rounded-full border-gray-300 bg-white">
+  <div className="relative h-10 w-10 rounded-full border-gray-300 bg-white overflow-hidden">
     <Image
     layout="fill"
-    src={`https://avatars.dicebear.com/api/open-peeps/${session?.user?.name || 'placeholder'}`}/>
-  </div>);
+    src={`https://avatars.dicebear.com/api/open-peeps/${session?.user?.name || 'placeholder'}.svg`}/>
+  </div>
+  );
 }
 
 export default Avatar;
--- components/Postbox.tsx
+++ components/Postbox.tsx
@@ -1,17 +1,27 @@
-import { useSession } from 'next-auth/react'
-import React from 'react'
-import Avatar from './Avatar';
+import { LinkIcon, PhotographIcon } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
+import React from "react";
+import Avatar from "./Avatar";
 
 function Postbox() {
-    const {data:session}=useSession();
+  const { data: session } = useSession();
   return (
     <form>
-      <Avatar/>
-        <div className='flex items-center space-x-3'>
-            <input className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none" disabled={!session} type="text" placeholder={session?'Create a post by entering a title!': 'Sign in to post'}/>
-        </div>
+      <div className="flex items-center space-x-3">
+        <Avatar />
+        <input
+          className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
+          disabled={!session}
+          type="text"
+          placeholder={
+            session ? "Create a post by entering a title!" : "Sign in to post"
+          }
+        />
+      </div>
+      <PhotographIcon className={`h-6 cursor-pointer text-gray-300`}/>
+      <LinkIcon className="h-6 text-gray-300"/>
     </form>
-  )
+  );
 }
 
-export default Postbox
+export default Postbox;
