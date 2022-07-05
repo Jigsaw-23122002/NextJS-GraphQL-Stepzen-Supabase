af7887c39d8eb6e7aab7368d86352ca9f0bc8a4c
Harsh <harshnag23@gmail.com>
2022-07-05 18:32:39 +0530

    Fourth Commit

---
+++ graphql/mutations.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+
+export const ADD_POST = gql`
+  mutation MyMutation(
+    $body: String!
+    $image: String!
+    $subreddit_id: ID!
+    $title: String!
+    $username: String!
+  ) {
+    insertPost(
+      body: $body
+      image: $image
+      subreddit: $subreddit
+      title: $title
+      username: $username
+    )
+    username
+    id
+    subreddit
+    title
+    body
+    created_at
+    image
+  }
+`;
+
+export const ADD_SUBREDDIT = gql`
+  mutation MyMutation($topic: String!) {
+    insertSubreddit(topic: $topic) {
+      id
+      topic
+      created_at
+    }
+  }
+`;
